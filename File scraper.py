9e78eb30d26fe8efc3a56008c3014129883a30f7
Shemaiah N’guessan <shemz@shems-m3-pro.home>
2024-11-01 08:55:29 +0000

    Add File scraper.py file

---
+++ File scraper.py
@@ -0,0 +1,9 @@
+
+# File organiser
+# Here is what this program will do:
+# - Scan the downloads folder for files
+# - Identifying files based on its type zip files, music projects, adobe projects, and more).
+# - Move the files into categorized folders (e.g., "Images", "Documents") based on file extensions.
+
+
+
